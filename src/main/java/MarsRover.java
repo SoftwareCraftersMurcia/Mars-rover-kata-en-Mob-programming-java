091ce2fed05aba3d1ca974d9953bb6c4c90315c9
carlos.castillo <carlos.castillo@cabify.com>
2020-10-30 18:57:51 +0100

    Add tests moving to north

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,6 +1,7 @@
 public class MarsRover {
 
     public String execute(java.lang.String m) {
+
         return "0:1:N";
     }
 
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -7,12 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MarsRoverTest {
 
 	@Test
-	public void give_me_a_good_name_please() {
+	public void going_one_step_to_north_should_move_to_0_1_N() {
 		MarsRover rover = new MarsRover();
 
 		String result = rover.execute("M");
 
-		assertEquals(true, result);
-		assertThat(result, equalTo(true));
+		assertEquals("0:1:N", result);
 	}
+
+	@Test
+	public void going_two_steps_to_north_should_move_to_0_2_N() {
+		MarsRover rover = new MarsRover();
+
+		String result = rover.execute("MM");
+
+		assertEquals("0:2:N", result);
+	}
+
 }
