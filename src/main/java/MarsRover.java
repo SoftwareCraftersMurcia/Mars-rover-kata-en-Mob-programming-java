8ba87542b05314a9ad4e26f93bd7f738adc16f99
Jahs <jahs.es@gmail.com>
2020-10-30 19:07:35 +0100

    jahs

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,14 +1,20 @@
 public class MarsRover {
+    private String orientation = "N";
 
     public String execute(java.lang.String command) {
         int number = 0;
         for (int i = 0; i < command.length(); i++) {
-           number++;
+            if (command.charAt(i) != "M") {
+                
+            }
+            number++;
         }
 
-        if (command == "R")
+        if (command == "R") {
+            orientation = "E";
+        }
 
-        return "0:"+ String.valueOf(number)+ ":N";
+        return "0:" + String.valueOf(number) + ":" + orientation;
     }
 
 }
