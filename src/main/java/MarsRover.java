0ad6a4a6853cf016c8142d8709b90f41e75f1c87
Luis Rovirosa Mairlot <luis@codium.team>
2020-10-31 16:15:56 +0100

    remove all the command creation

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,29 +1,26 @@
+import java.util.Map;
+
 public class MarsRover {
     public static final int LEFT_ROTATION = -1;
     private Position position;
+    private final Map<Character, MarsCommand> commands;
 
     public MarsRover() {
         position = new Position(0, 0, Orientation.North);
+        commands = Map.of(
+                'M', new MoveForward(),
+                'R', new RotateRight(),
+                'L', new RotateLeft()
+        );
     }
 
     public String execute(String commands) {
         for (int i = 0; i < commands.length(); i++) {
-            MarsCommand command = createCommand(commands.charAt(i));
+            MarsCommand command = this.commands.get(commands.charAt(i));
             command.execute(position);
         }
 
         return position.toString();
-    }
-
-    private MarsCommand createCommand(char charCommand) {
-        if (charCommand == 'M') {
-            return new MoveForward();
-        } else if ('R' == charCommand) {
-            return new RotateRight();
-        } else if ('L' == charCommand) {
-            return new RotateLeft();
-        }
-        throw new IllegalArgumentException("The command " + charCommand + " is not recognized");
     }
 
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,13 +1,7 @@
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
