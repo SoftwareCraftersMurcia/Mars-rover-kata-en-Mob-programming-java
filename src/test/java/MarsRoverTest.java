45ba6f2dea8528a8b8de37f7b7cc6da550faaef3
Luis Rovirosa Mairlot <luis@codium.team>
2020-10-31 10:05:31 +0100

    from_north_going_right_four_times_should_face_north

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -52,6 +52,15 @@ class MarsRoverTest {
     }
 
     @Test
+    public void from_north_going_right_four_times_should_face_north() {
+        MarsRover rover = new MarsRover("N");
+
+        String result = rover.execute("RRRR");
+
+        assertEquals("0:0:N", result);
+    }
+
+    @Test
     public void from_west_going_right_should_face_north() {
         MarsRover rover = new MarsRover("W");
 
