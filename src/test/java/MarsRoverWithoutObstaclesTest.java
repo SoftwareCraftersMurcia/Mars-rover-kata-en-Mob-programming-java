293a1ab102dcb157f3800b9553685aa7f42a68fd
Luis Rovirosa Mairlot <luis@codium.team>
2020-11-01 09:57:41 +0100

    rename test to make space for obstacles

--- src/test/java/MarsRoverWithoutObstaclesTest.java
+++ src/test/java/MarsRoverWithoutObstaclesTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MarsRoverTest {
+class MarsRoverWithoutObstaclesTest {
 
     private MarsRover rover;
 
