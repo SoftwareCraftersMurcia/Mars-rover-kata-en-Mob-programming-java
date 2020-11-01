6067ebb8fe2846dc8ac9cec4fe76f4b3c212a2ea
pascual <pascual@strata3.com>
2020-11-01 21:23:06 +0100

    Complete tests for advance in any direction

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,7 +1,30 @@
 public class MarsRover {
 
-    public String execute(java.lang.String m) {
-        return "0:1:N";
+    private String heading;
+
+    public MarsRover() {
+        this.heading = "N";
     }
+
+    public MarsRover(String heading) {
+        this.heading = heading;
+    }
+
+    public String execute(java.lang.String commands) {
+        switch (heading) {
+            case "N":
+                return "0:1:N";
+            case "E":
+                return "1:0:E";
+            case "S":
+                return "0:-1:S";
+            case "W":
+                return "-1:0:W";
+            default:
+                return "0:0:N";
+        }
+
+    }
+
 
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -7,12 +7,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MarsRoverTest {
 
 	@Test
-	public void give_me_a_good_name_please() {
+	public void going_one_step_to_north_should_move_to_0_1_N() {
 		MarsRover rover = new MarsRover();
 
 		String result = rover.execute("M");
 
-		assertEquals(true, result);
-		assertThat(result, equalTo(true));
+		assertEquals("0:1:N", result);
 	}
+
+	@Test
+	public void going_one_step_to_east_should_move_to_1_0_N() {
+		MarsRover rover = new MarsRover("E");
+
+		String result = rover.execute("M");
+
+		assertEquals("1:0:E", result);
+	}
+
+	@Test
+	public void going_one_step_to_south_should_move_to_0_minus_1_N() {
+		MarsRover rover = new MarsRover("S");
+
+		String result = rover.execute("M");
+
+		assertEquals("0:-1:S", result);
+	}
+
+	@Test
+	public void going_one_step_to_west_should_move_to_minus_1_0_N() {
+		MarsRover rover = new MarsRover("S");
+
+		String result = rover.execute("M");
+
+		assertEquals("0:-1:S", result);
+	}
+
+
 }
