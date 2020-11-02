28a2c09efe408ee5389ecffd186f0004d215f206
Juan Sánchez <juan.sanchez@xepient.com>
2020-11-02 14:15:42 +0100

    juan-sanchez-montesinos - Add tests to cover rotating

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -40,5 +42,45 @@ class MarsRoverTest {
 		String result = rover.execute("LLLLR");
 
 		assertEquals("0:0:E", result);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"", "LLLL", "RRRR", "LLLLRRRR", "LLLLLLLL"})
+	public void should_face_north(String commands) {
+		MarsRover rover = new MarsRover();
+
+		String result = rover.execute(commands);
+
+		assertEquals("0:0:N", result);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"LL", "RR", "LLRRRR", "RRRRRR"})
+	public void should_face_south(String commands) {
+		MarsRover rover = new MarsRover();
+
+		String result = rover.execute(commands);
+
+		assertEquals("0:0:S", result);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"R", "RRRRR", "LLL"})
+	public void should_face_east(String commands) {
+		MarsRover rover = new MarsRover();
+
+		String result = rover.execute(commands);
+
+		assertEquals("0:0:E", result);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"L", "LLLLL", "RRR"})
+	public void should_face_west(String commands) {
+		MarsRover rover = new MarsRover();
+
+		String result = rover.execute(commands);
+
+		assertEquals("0:0:W", result);
 	}
 }
